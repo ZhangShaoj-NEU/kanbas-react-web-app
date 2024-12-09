5dcf336e8c64270635b4f66dac82cf706948137f
ZhangShaoj-NEU <zhang.shaoj@northeastern.edu>
2024-12-09 01:35:32 -0800

    Update client.ts
    fix bug

--- src/Kanbas/Account/client.ts
+++ src/Kanbas/Account/client.ts
@@ -28,8 +28,8 @@ export const findMyCourses = async () => {
     return data;
 };
 
-export const createCourse = async (course: any) => {
-    const { data } = await axiosWithCredentials.post(`${USERS_API}/current/courses`, course);
+export const createCourse = async (user: any, course: any) => {
+    const { data } = await axiosWithCredentials.post(`${USERS_API}/${user._id}/courses`, course);
     return data;
 };
   
